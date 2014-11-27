8625e9ebb197319f95f888dfb01ba3dcf67fb275
danielk7 <danyl.kovalyov@gmail.com>
2014-11-27 00:19:47 -0800

    Version 2

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,14 @@ $(document).ready(function() {
 
 
     success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.title+'&deg;'+weather.units.temp+'</h2>';
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.city+'&deg;'+weather.units.temp+'</h2>';
 
       console.log
 
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<ul><li>'+weather.temp+' '+weather.units.temp+'</li>';
+      html += '<li class="currently">'+weather.city+', '+weather.region+'</li>';
+      html += '<li>'+weather.currently+'</li>';
       html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
-  
   // PUT DISPLAY
   $('#weather').html(html);
 
