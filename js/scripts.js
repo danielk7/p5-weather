510ff66437803e35264cc305ba2dc35fec393444
danielk7 <danyl.kovalyov@gmail.com>
2014-12-06 03:06:19 -0800

    Version 6

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var cheney = '99004';
-var spokane = '99201';
+var spokane = '99201';'99202';'99203';'99204'; '99205'; '99206';'99207';
+var seattle = '98101';
 
 
 $.simpleWeather({
@@ -14,8 +15,13 @@ $.simpleWeather({
       $('.current').text(weather.currently);
       $('.header').text(weather.city);
       $('.pressure').text(weather.pressure);
+      $('.wind').text(weather.wind.direction).text(weather.speed);
       
       console.log(weather);
+
+      for(var i=0;i<weather.forecast.length;i++) {
+        html += '<p>'+weather.forecast[i].day+': '+weather.forecast[i].high+'</p>';
+      }
 
     },
     
@@ -23,4 +29,37 @@ $.simpleWeather({
       $("#weather").html('<p>'+error+'</p>');
     }
     
+  });
+
+
+
+
+// Defoult
+ // Remove existing class
+$('body').addClass('day');
+
+// Click 'Day' change to day
+$('#day').click( function(){
+  
+  // Remove existing class
+  $('body').removeClass();
+  
+  // Add class of 'Day' to the <body>
+  $('body').addClass('day');
+  
+  // console.log('Click on Day link');
+  
+});
+
+// Click 'Night' change to night
+$('#night').click( function(){
+  
+   // Remove existing class
+  $('body').removeClass();
+  
+  // Add class of 'Day' to the <body>
+  $('body').addClass('night');
+  
+  // console.log('Click on Day link');
+  
   });
