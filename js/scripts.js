4ef65229845b4f4f1efe784d0b415093e647087b
danielk7 <danyl.kovalyov@gmail.com>
2014-12-11 02:11:58 -0800

    Version 14

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,47 @@ var spokane = '99201';'99202';'99203';'99204'; '99205'; '99206';'99207';
 var seattle = '98101';
 
 
+// REF: http://foundation.zurb.com/docs/
+// REF: http://simpleweatherjs.com/
+
+// On click button, get zip, then run Simple Weather
+$('button').on('click', function() {
+  
+  // 1. Get & store entered zipcode
+  var zipcode = $('#getWeather').val();
+  
+  // 2. Pass weather into _simpleWeather()_ object
+  $.simpleWeather({
+    
+    location: zipcode,
+  
+    success: function(weather) {
+      
+      // Get & store temperature
+      var temp = weather.temp;
+      // Get & store city
+      var city = weather.city;
+      
+      // Output to hooks in HTML
+      $('.temp').text(temp);
+      $('.city').text(city);
+
+      // See console for all properties of object
+      console.log(weather);
+    },
+  
+    error: function(error) {
+      $('body').html('<p>' + error + '</p>');
+    }
+  
+  });
+  
+  // 3. Reset input value
+  $('#getWeather').val('');
+  
+});
+
+
  $.simpleWeather({
     
     location: spokane,
